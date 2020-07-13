3014a33b145dbdd404baa7c4d3411d23728e8960
Châu <tranhoaichau.00@gmail.com>
2020-07-13 22:12:45 +0700

    add nav bar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import '../../css/App.css';
-import "../../css/animate.css"
-
-import"../../css/owl.carousel.min.css"
-import"../../css/owl.theme.default.min.css"
-import"../../css/magnific-popup.css"
-import"../../css/flaticon.css"
-import"../../css/style.css"
+import Header from '../../Components/Header'
+import SectionHome from './sectionHome'
+import './style.css'
 function Home() {
   return (
-    <div className="Home">
-      hello
+    <div id="Home">
+      <Header/>
+      <div id="section-container">
+        <SectionHome/>
+        <section className="child-section" style={{width:'100%', height:'60vh'}}>
+          section 2
+        </section>
+        <section className="child-section" style={{width:'100%', height:'60vh', backgroundColor:'yellow'}}>
+          section 3
+        </section>
+      </div>
+      
+
     </div>
   );
 }
