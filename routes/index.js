176854e0df651f0ed2dcfef9bd9cc2d42320c6fc
Châu <tranhoaichau.00@gmail.com>
2020-07-19 01:01:07 +0700

    complete install sql

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router()
-const googleSpreadSheet = require('google-spreadsheet')
-const {promisify} = require('util')
-const creds = require('../MyProject-eda84eaa3467.json')
+const models = require("../models/")
+
 
 router.get("/hello", (req, res) => {
     res.send("hi")
 })
 
-router.post('/add-sheet-data', async (req, res) => {
-    const doc = new googleSpreadSheet('1cm6SX14gc9EYURjraITh5_WdSfxzEMmZz_c2030fnys')
-    await promisify(doc.useServiceAccountAuth)(creds)
+router.get("/hi", async (req,res) => {
+    const test = await models.Event.findAll();
+    console.log(test)
+    res.send("hi")
 })
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 const router = require('./routes')
-const { Sequelize } = require('sequelize');
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
@@ -15,24 +14,8 @@ app.get('/ping', function (req, res) {
 });
 app.use('/v1', router)
 
-let sequelize
-if (process.env.NODE_ENV === 'production') {
-  sequelize = new Sequelize(process.env.POSTGRE_PRODUCTION_URL)
-}
-else {
-  sequelize = new Sequelize(process.env.POSTGRE_LOCAL_URL)
-}
 
 
-async function test () {
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-}
-test();
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
