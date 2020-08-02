be5be2c5e460679abd92cc22d360807f5a30cd15
katoo <tctung18@apcs.vn>
2020-08-02 12:30:59 +0700

    about-Section

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import Header from '../../Components/Header'
 import SectionHome from './SectionHome'
-import SectionMA from './SectionMA'
-import SectionRule from './SectionRule'
-import SectionAward from './SectionAward'
-import SectionNote from './SectionNote'
-import SectionRegister from './SectionRegister'
-
+import SectionAbout from './SectionAbout'
 import './style.scss'
 
 export default function Home() {
   return (
     <div id="Home">
+     
       <Header/>
       <SectionHome/>
-      <SectionMA/>
-      <SectionRule/>
-      <div id='bended-content'>
-        <SectionAward/>
-        <SectionNote/>
-        <SectionRegister/>
-      </div>
+      <SectionAbout/>
+
     </div>
   );
 }
